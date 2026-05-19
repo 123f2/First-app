276be34571a1f01504f302b55501c516f2438425
123f2 <148834071+123f2@users.noreply.github.com>
2026-05-19 14:34:21 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+import streamlit as st
+ 
+st.write ( """
+# My first app
+Hello *world!*
+
+What's your name?   
+                 
+I love you very much!  
+
+How about the weather today?
+
+""")
